3217b56d97edc0decae738cd66bccf18b106795d
Artem Kungurov <a.kungurov@pixonic.com>
2022-04-25 16:00:38 +0300

    Changed eclipse card add order

--- Assets/Scripts/Widgets_new/Eclipse/CardsContainer/PlaceWidgetEclipseLayout.cs
+++ Assets/Scripts/Widgets_new/Eclipse/CardsContainer/PlaceWidgetEclipseLayout.cs
@@ -9,6 +9,26 @@ namespace Solcery.Widgets_new.Eclipse.CardsContainer
         [SerializeField]
         private ScrollRect scroll;
 
+        [SerializeField]
+        private HorizontalLayoutGroup horizontalLayoutGroup;
+
+        public void SetAnchor(TextAnchor anchor)
+        {
+            horizontalLayoutGroup.childAlignment = anchor;
+            switch (anchor)
+            {
+                case TextAnchor.MiddleLeft:
+                    scroll.content.pivot = new Vector2(0f, 1f);
+                    break;
+                case TextAnchor.MiddleRight:
+                    scroll.content.pivot = new Vector2(1f, 0f);
+                    break;
+                case TextAnchor.MiddleCenter:
+                    scroll.content.pivot = new Vector2(0.5f, 0.5f);
+                    break;
+            }
+        }
+
         public void AddCard(IEclipseCardInContainerWidget eclipseCardInContainerWidget)
         {
             eclipseCardInContainerWidget.UpdateParent(scroll.content);
