d444947ed433aedc19d98d49ae0935a5b3c6eb38
Sergei Gladkov <opensr88@gmail.com>
2022-08-09 10:36:18 +0300

    Обновил лейаут у nft selector

--- Assets/Scripts/Widgets_new/Eclipse/Nft/Selector/PlaceWidgetEclipseNftSelectorLayout.cs
+++ Assets/Scripts/Widgets_new/Eclipse/Nft/Selector/PlaceWidgetEclipseNftSelectorLayout.cs
@@ -1,7 +1,45 @@
+using Solcery.Widgets_new.Eclipse.Nft.Card;
+using UnityEngine;
+using UnityEngine.UI;
+
 namespace Solcery.Widgets_new.Eclipse.Nft.Selector
 {
     public sealed class PlaceWidgetEclipseNftSelectorLayout : PlaceWidgetLayout
     {
+        [SerializeField]
+        private ScrollRect scroll;
+
+        [SerializeField]
+        private HorizontalLayoutGroup horizontalLayoutGroup;
         
+        public void SetAnchor(TextAnchor anchor)
+        {
+            horizontalLayoutGroup.childAlignment = anchor;
+            switch (anchor)
+            {
+                case TextAnchor.MiddleLeft:
+                    scroll.content.pivot = new Vector2(0f, 1f);
+                    break;
+                case TextAnchor.MiddleRight:
+                    scroll.content.pivot = new Vector2(1f, 0f);
+                    break;
+                case TextAnchor.MiddleCenter:
+                    scroll.content.pivot = new Vector2(0.5f, 0.5f);
+                    break;
+            }
+        }
+
+        public void AddCard(IEclipseCardNftInContainerWidget inContainerWidget)
+        {
+            inContainerWidget.UpdateParent(scroll.content);
+            inContainerWidget.UpdateSiblingIndex(inContainerWidget.Order);
+            scroll.horizontal = scroll.content.childCount > 1;
+            Rebuild();
+        }
+
+        public void Rebuild()
+        {
+            LayoutRebuilder.ForceRebuildLayoutImmediate(scroll.content);
+        }
     }
 }
