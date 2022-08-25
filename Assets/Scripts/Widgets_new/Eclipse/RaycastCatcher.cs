774414a62ddfe2fd081386415b55924bc459b2ab
Sergei Gladkov <opensr88@gmail.com>
2022-08-25 19:31:31 +0300

    Fix touch catch

---
+++ Assets/Scripts/Widgets_new/Eclipse/RaycastCatcher.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Solcery.Widgets_new.Eclipse
+{
+    [RequireComponent(typeof(CanvasRenderer))]
+    public sealed class RaycastCatcher : Graphic { }
+}
